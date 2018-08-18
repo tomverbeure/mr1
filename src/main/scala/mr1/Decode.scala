3385c6a564c730ce593413a1397df29a9b917fb0
Tom Verbeure <hombre+github@gmail.com>
2018-08-18 00:00:34 -0700

    InstrType & InstrFormat

--- src/main/scala/mr1/Decode.scala
+++ src/main/scala/mr1/Decode.scala
@@ -3,7 +3,7 @@ package mr1
 
 import spinal.core._
 
-object InstrType extends SpinalEnum {
+object InstrFormat extends SpinalEnum {
     val Undef   = newElement()
     val R       = newElement()
     val I       = newElement()
@@ -13,34 +13,37 @@ object InstrType extends SpinalEnum {
     val J       = newElement()
 }
 
-case class DecodedInstr(hasMul: Boolean, hasDiv: Boolean, hasCsr: Boolean) extends Bundle {
+object InstrType extends SpinalEnum {
+    val LUI     = newElement()
+    val AUIPC   = newElement()
+    val JAL     = newElement()
+    val JALR    = newElement()
+    val B       = newElement()
+    val L       = newElement()
+    val S       = newElement()
+    val ALU_I   = newElement()
+    val SHIFT_I = newElement()
+    val ALU     = newElement()
+    val SHIFT   = newElement()
+    val FENCE   = newElement()
+    val E       = newElement()
+    val CSR     = newElement()
+    val MUL     = newElement()
+    val DIV     = newElement()
+}
 
+case class DecodedInstr(config: MR1Config) extends Bundle {
+
+    val iformat = InstrFormat()
     val itype   = InstrType()
-
-    val lui     = Bool
-    val auipc   = Bool
-    val jal     = Bool
-    val jalr    = Bool
-    val b       = Bool
-    val l       = Bool
-    val s       = Bool
-    val alu_i   = Bool
-    val shift_i = Bool
-    val alu     = Bool
-    val shift   = Bool
-    val fence   = Bool
-    val e       = Bool
-    val csr     = if (hasCsr) Bool else null
-    val mul     = if (hasMul) Bool else null
-    val div     = if (hasDiv) Bool else null
 }
 
 
-class Decode extends Component {
+class Decode(config: MR1Config) extends Component {
 
-    val hasMul = true
-    val hasDiv = true
-    val hasCsr = false
+    val hasMul = config.hasMul
+    val hasDiv = config.hasMul
+    val hasCsr = config.hasMul
 
     val io = new Bundle {
         val instr_valid = in Bool
@@ -62,85 +65,64 @@ class Decode extends Component {
     val u_imm_31_12 = io.instr(31 downto 12)
     val j_imm_20_1  = io.instr(20) ## io.instr(19 downto 12) ## io.instr(20) ## io.instr(30 downto 21)
 
-    val decoded_instr       = DecodedInstr(hasMul, hasDiv, hasCsr)
+    val decoded_instr       = DecodedInstr(config)
 
-    decoded_instr.itype     := InstrType.Undef
-    decoded_instr.lui       := False
-    decoded_instr.auipc     := False
-    decoded_instr.jal       := False
-    decoded_instr.jalr      := False
-    decoded_instr.b         := False
-    decoded_instr.l         := False
-    decoded_instr.s         := False
-    decoded_instr.alu_i     := False
-    decoded_instr.shift_i   := False
-    decoded_instr.alu       := False
-    decoded_instr.shift     := False
-    decoded_instr.fence     := False
-    decoded_instr.e         := False
-    if (hasCsr){
-        decoded_instr.csr       := False
-    }
-    if (hasMul){
-        decoded_instr.mul       := False
-    }
-    if (hasDiv){
-        decoded_instr.div       := False
-    }
+    decoded_instr.iformat   := InstrFormat.Undef
+    decoded_instr.itype     := InstrType.LUI
 
     switch(opcode){
         // LUI
         is(B"0110111"){
-            decoded_instr.lui       := True
-            decoded_instr.itype     := InstrType.U
+            decoded_instr.itype     := InstrType.LUI
+            decoded_instr.iformat   := InstrFormat.U
         }
         // AUIPC
         is(B"0010111"){
-            decoded_instr.auipc     := True
-            decoded_instr.itype     := InstrType.U
+            decoded_instr.itype     := InstrType.AUIPC
+            decoded_instr.iformat   := InstrFormat.U
         }
         // JAL
         is(B"1101111"){
-            decoded_instr.jal       := True
-            decoded_instr.itype     := InstrType.J
+            decoded_instr.itype     := InstrType.JAL
+            decoded_instr.iformat   := InstrFormat.J
         }
         // JALR
         is(B"1100111"){
             when(funct3 === B"000") {
-                decoded_instr.jalr      := True
-                decoded_instr.itype     := InstrType.I
+                decoded_instr.itype     := InstrType.JALR
+                decoded_instr.iformat   := InstrFormat.I
             }
         }
         // Bxx
         is(B"1100011"){
             when(funct3 =/= B"010" && funct3 =/= B"011") {
-                decoded_instr.b         := True
                 decoded_instr.itype     := InstrType.B
+                decoded_instr.iformat   := InstrFormat.B
             }
         }
         // Lxx
         is(B"0000011"){
             when(funct3 =/= B"010" && funct3 =/= B"011" && funct3 =/= B"110" && funct3 =/= B"111") {
-                decoded_instr.l         := True
-                decoded_instr.itype     := InstrType.I
+                decoded_instr.itype     := InstrType.L
+                decoded_instr.iformat   := InstrFormat.I
             }
         }
         // Sx
         is(B"0100011"){
             when(funct3 === B"000" || funct3 === B"001" || funct3 === B"010") {
-                decoded_instr.s         := True
                 decoded_instr.itype     := InstrType.S
+                decoded_instr.iformat   := InstrFormat.S
             }
         }
         is(B"0010011"){
             when(funct3 === B"000" || funct3 === B"010" || funct3 === B"011" || funct3 === B"100" || funct3 === B"110" || funct3 === B"111") {
                 // ALU_I
-                decoded_instr.alu_i     := True
-                decoded_instr.itype     := InstrType.I
+                decoded_instr.itype     := InstrType.ALU_I
+                decoded_instr.iformat   := InstrFormat.I
             }.elsewhen( (funct7 ## funct3) === B"0000000001" || (funct7 ## funct3) === B"0000000101" || (funct7 ## funct3) === B"0100000101") {
                 // SHIFT_I
-                decoded_instr.shift_i   := True
-                decoded_instr.itype     := InstrType.R
+                decoded_instr.itype     := InstrType.SHIFT_I
+                decoded_instr.iformat   := InstrFormat.R
             }
         }
         // ALU, SHIFT
@@ -148,25 +130,25 @@ class Decode extends Component {
             switch(funct7 ## funct3){
                 is(B"0000000_000", B"0100000_000", B"0000000_100", B"0000000_110", B"0000000_111"){
                     // ADD, SUB, XOR, OR, AND
-                    decoded_instr.alu       := True
-                    decoded_instr.itype     := InstrType.R
+                    decoded_instr.itype     := InstrType.ALU
+                    decoded_instr.iformat   := InstrFormat.R
                 }
                 is(B"0000000_001", B"0000000_101", B"0100000_101"){
                     // SLL, SRL, SRA
-                    decoded_instr.shift     := True
-                    decoded_instr.itype     := InstrType.R
+                    decoded_instr.itype     := InstrType.SHIFT
+                    decoded_instr.iformat   := InstrFormat.R
                 }
                 is( B"0000000_010", B"0000000_011") {
                     // SLT, SLTU
-                    decoded_instr.alu       := True
-                    decoded_instr.itype     := InstrType.R
+                    decoded_instr.itype     := InstrType.ALU
+                    decoded_instr.iformat   := InstrFormat.R
                 }
                 is(B"0000001_000", B"0000001_001", B"0000001_010", B"0000001_011"){
                     // MUL
                     if (hasMul){
                         when(funct7 === B"0000001"){
-                            decoded_instr.mul       := True
-                            decoded_instr.itype     := InstrType.R
+                            decoded_instr.itype     := InstrType.MUL
+                            decoded_instr.iformat   := InstrFormat.R
                         }
                     }
                 }
@@ -174,8 +156,8 @@ class Decode extends Component {
                     // DIV
                     if (hasDiv){
                         when(funct7 === B"0000001"){
-                            decoded_instr.div       := True
-                            decoded_instr.itype     := InstrType.R
+                            decoded_instr.itype     := InstrType.DIV
+                            decoded_instr.iformat   := InstrFormat.R
                         }
                     }
                 }
@@ -184,20 +166,20 @@ class Decode extends Component {
         // FENCE
         is(B"0001111"){
             when( funct3 === B"000" || funct3 === B"001"){
-                decoded_instr.fence     := True
-                decoded_instr.itype     := InstrType.I
+                decoded_instr.itype     := InstrType.FENCE
+                decoded_instr.iformat   := InstrFormat.I
             }
         }
         // ECALL, EBREAK, CSR
         is(B"1110011"){
             when( io.instr(31 downto 7) === B"0000_0000_0000_0000_0000_0000_0" || io.instr(31 downto 7) === B"0000_0000_0001_0000_0000_0000_0")
             {
-                decoded_instr.e         := True
-                decoded_instr.itype     := InstrType.I
+                decoded_instr.itype     := InstrType.E
+                decoded_instr.iformat   := InstrFormat.I
             }.elsewhen(funct3 === B"001" || funct3 === B"010" || funct3 === B"011" || funct3 === B"101" || funct3 === B"110" || funct3 === B"111") {
                 if (hasCsr){
-                    decoded_instr.csr       := True
-                    decoded_instr.itype     := InstrType.I
+                    decoded_instr.itype     := InstrType.CSR
+                    decoded_instr.iformat   := InstrFormat.I
                 }
             }
         }
--- src/main/scala/mr1/MR1.scala
+++ src/main/scala/mr1/MR1.scala
@@ -3,13 +3,24 @@ package mr1
 
 import spinal.core._
 
-class MR1 extends Component {
+case class MR1Config(
+                supportMul      : Boolean = true,
+                supportDiv      : Boolean = true,
+                supportCsr      : Boolean = true) {
+
+    def hasMul = supportMul
+    def hasDiv = supportDiv
+    def hasCsr = supportCsr
+}
+
+
+class MR1(config: MR1Config) extends Component {
     val io = new Bundle {
         val instr_valid = in Bool
         val instr       = in Bits(32 bits)
     }
 
-    val decode = new Decode()
+    val decode = new Decode(config)
 
     io.instr_valid <> decode.io.instr_valid
     io.instr <> decode.io.instr
@@ -18,7 +29,7 @@ class MR1 extends Component {
 
 object MR1Verilog {
     def main(args: Array[String]) {
-        SpinalVerilog(new MR1)
+        SpinalVerilog(new MR1(config = MR1Config()))
     }
 }
 
